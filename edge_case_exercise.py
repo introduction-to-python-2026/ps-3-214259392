de0a632c1d2aab9adcd68b2f88a090e2d0a1b3eb
214259392 <tiferetw@gmail.com>
2025-11-08 15:39:16 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -17,5 +17,5 @@ def approximate_pi(n):
     total = 0
     for i in range(n):
         total += ((-1) ** i) / (2 * i + 1)
-    leibniz_series = total *4    
-    return leibniz_series
+    return 4 * total
+
