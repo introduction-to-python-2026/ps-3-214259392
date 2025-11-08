eaad7a226c19feba4e71a428c7a41b2a14e413dc
214259392 <tiferetw@gmail.com>
2025-11-08 15:35:21 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -12,3 +12,10 @@ def move(my_list, direction):
       my_list[index_of_one - 1] = 1
 
   return my_list
+
+def approximate_pi(n):
+    total = 0
+    for i in range(n):
+        total += ((-1) ** i) / (2 * i + 1)
+    leibniz_series = total *4    
+    return leibniz_series
